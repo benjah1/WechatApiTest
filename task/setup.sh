cf57e3447655f27e0d57a4c0841b34bd8a8c39ba
benjah1 <benjaminhuang1@gmail.com>
2015-11-09 02:02:45 +0000

    update 1) ali source with different regex 2) use docker directly

--- task/setup.sh
+++ task/setup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 echo "Update source"
-# sed -i 's/archive.ubuntu.com/mirrors.aliyun.com/g' /etc/apt/sources.list
+sed -i 's/\/\/.*archive.ubuntu.com/mirrors.aliyun.com/g' /etc/apt/sources.list
 apt-get update
 
 echo "Check docker"
@@ -17,16 +17,10 @@ if [ -z "`which docker-compose`" ]; then
 fi
 
 echo "Check docker client"
-DOCKER="dao"
-
-if [ -z "`which dao`" ]; then
-  $DOCKER="docker"
-fi
-
-$DOCKER version
+docker version
 
 echo "Pull docker image"
-$DOCKER pull daocloud.io/library/php:5.6-fpm
-$DOCKER pull daocloud.io/library/mysql:5.6
-$DOCKER tag daocloud.io/library/php:5.6-fpm php:5.6-fpm
-$DOCKER tag daocloud.io/library/mysql:5.6 mysql:5.6
+docker pull daocloud.io/library/php:5.6-fpm
+docker pull daocloud.io/library/mysql:5.6
+docker tag daocloud.io/library/php:5.6-fpm php:5.6-fpm
+docker tag daocloud.io/library/mysql:5.6 mysql:5.6
